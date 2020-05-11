9d79311698cd4350fb847a11a4595c5b91877e6b
kalenkovich <e.kalenkovich@gmail.com>
2020-05-11 14:13:43 +0300

    Plot trajectories

--- mousetracking.js
+++ mousetracking.js
@@ -25,7 +25,37 @@ var mousetracking = {
 	
 	stop_tracking: function(){
 		$(document).off('mousemove');
-		console.log('Stopped tracking');		
+		console.log('Stopped tracking');	
+		mousetracking.plot_trajectory();
 	},
+	
+	plot_trajectory: function(){
+		plotting.make_canvas();
+		for (coords of mousetracking.trajectory) {
+			plotting.add_point(coords.x, coords.y);
+		}
+	}
+}
 
+var plotting = {
+	make_canvas: function(){
+		if (typeof plotting.canvas !== 'undefined') {
+			return;
+		}
+		
+		var canvas = document.createElement("canvas");
+		plotting.canvas = canvas;
+		
+		canvas.setAttribute("style", "z-index:-1; display: block; width: 100%; height: 100%;");
+		
+		document.body.appendChild(canvas);
+	},
+	
+	add_point: function(x, y){
+		var ctx = plotting.canvas.getContext("2d");
+		var rect = plotting.canvas.getBoundingClientRect();
+		x_ = (x - rect.left) / (rect.right - rect.left) * plotting.canvas.width,
+		y_ = (y - rect.top) / (rect.bottom - rect.top) * plotting.canvas.height
+		ctx.fillRect(Math.round(x_), Math.round(y_), 1, 1);
+	}
 }
