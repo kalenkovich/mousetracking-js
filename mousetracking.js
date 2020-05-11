ab63c4b9aeb7c634c018dbfae3a01fe5a5cc05c8
kalenkovich <e.kalenkovich@gmail.com>
2020-05-11 12:29:06 +0300

    Print normalized coordinates to console

--- mousetracking.js
+++ mousetracking.js
@@ -1,9 +1,23 @@
 var mousetracking = {
+	normalize_coordinates: function(x, y){
+		// bottom center is (0,0), top right is (1, 1), top left is (-1,1)
+		width = $(window).width();
+		height = $(window).height();
+		x_normalized = x / width - 0.5;
+		y_normalized = 1- y / height;
+		return [x_normalized, y_normalized];
+	}, 
+	
 	start_tracking: function(){
 		console.log('Started tracking');
+		$(document).mousemove(function(event){
+			console.log(mousetracking.normalize_coordinates(event.clientX, event.clientY));
+		});
 	},
 	
 	stop_tracking: function(){
-		console.log('Stopped tracking');
-	}
+		$(document).off('mousemove');
+		console.log('Stopped tracking');		
+	},
+
 }
